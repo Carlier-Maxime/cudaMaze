04b9bfff76f70b34338ef2a7a797548f82cc7945
maxime_carlier <28m06c02@gmail.com>
2025-05-06 14:45:41 +0200

    use inline func for get array indices

--- src/cuda/array.cuh
+++ src/cuda/array.cuh
@@ -1,8 +1,25 @@
 #pragma once
+#include <vector>
+#include <iostream>
+
+__device__ __forceinline__ size_t d_getArray1DIndex() {
+    return blockIdx.x * blockDim.x + threadIdx.x;
+}
+
+struct Array2DIndices {
+    const size_t y, x, i;
+};
+
+__device__ __forceinline__ Array2DIndices d_getArray2DIndices(const size_t width) {
+    const auto y = blockIdx.y * blockDim.y + threadIdx.y;
+    const auto x = blockIdx.x * blockDim.x + threadIdx.x;
+    const auto i = y * width + x;
+    return {y, x, i};
+}
 
 template <typename GRID_TYPE>
 __global__ void kernelInitArrayRange1D(GRID_TYPE *grid, const size_t size, GRID_TYPE start, GRID_TYPE increment) {
-    const auto i = blockIdx.x * blockDim.x + threadIdx.x;
+    const auto i = d_getArray1DIndex();
     if (i < size) grid[i] = start + i*increment;
 }
 
--- src/cuda/random.cu
+++ src/cuda/random.cu
@@ -1,9 +1,11 @@
 #include "random.cuh"
 
+#include "array.cuh"
+
 __global__ void kernelInitCurand(const size_t seed, curandState *states, const size_t size) {
     if (const auto id = blockIdx.x * blockDim.x + threadIdx.x; id < size) curand_init(seed, id, 0, &states[id]);
 }
 
 __global__ void kernelRandomArray(uint32_t *arr, curandState *states, const size_t size) {
-    if (const auto i = blockIdx.x * blockDim.x + threadIdx.x; i < size) arr[i] = curand(states+i);
+    if (const auto i = d_getArray1DIndex(); i < size) arr[i] = curand(states+i);
 }
