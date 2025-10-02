0e365bbda8dd0b9bf12bcc47e7e8d11d87380323
M_EtOuais <28m06c02@gmail.com>
2025-10-02 18:55:46 +0200

    :art: enhance maze logging, add size check for print, and measure solve/save durations

--- src/main.cu
+++ src/main.cu
@@ -15,10 +15,11 @@ int main() {
     std::cout << "height : ";
     std::cin >> h;
     auto maze = Maze::make<BackendCUDA>(h, w, time(nullptr), true);
-    const auto chrono = Chronometer();
+    auto chrono = Chronometer();
     maze.toPNG<BackendCUDA>("maze.png");
     std::cout << "Save Maze to PNG in : " << chrono << std::endl;
-    std::cout << maze << std::endl;
+    if (maze.getSize() < 1024) std::cout << maze << std::endl;
+    else std::cout << "maze not print because size is big (" << maze.getSize() << ')' << std::endl;
     MazeSolution<size_t> solution = {
         {},
         {},
@@ -27,11 +28,15 @@ int main() {
         false,
         0
     };
+    chrono.reset();
     MazeResolver<size_t, BackendCUDA>().resolve(maze, solution);
+    std::cout << "Solve Maze in : " << chrono << std::endl;
+    chrono.reset();
     std::vector<char> pathValues(solution.maxDistance);
     for (size_t i = 0; i < solution.maxDistance; ++i) {
         pathValues[i] = 32 + i*223 / solution.maxDistance;
     }
     maze.toPNG<size_t, BackendCUDA>("maze_solve.png", 3, 3, solution.distanceToEnd, pathValues);
+    std::cout << "Save maze solve to PNG in : " << chrono << std::endl;
     return EXIT_SUCCESS;
 }
